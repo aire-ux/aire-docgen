6d08a649e3d69d09b63511f0d9ffb776ac7026bf
Josiah Haswell <josiah.haswell@gmail.com>
2021-06-08 14:54:08 -0600

    Adding initial layout and project

--- docgen-ui/src/main/java/com/example/demo/MainView.java
+++ docgen-ui/src/main/java/com/example/demo/MainView.java
@@ -1,45 +1,24 @@
 package com.example.demo;
 
-import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
-/**
- * The main view contains a button and a click listener.
- */
 @Route("")
 @PWA(name = "Project Base for Vaadin", shortName = "Project Base")
 @CssImport("./styles/shared-styles.css")
-@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
-public class MainView extends VerticalLayout implements AppShellConfigurator {
+@CssImport(value = "./styles/application.css")
+public class MainView extends AppLayout implements AppShellConfigurator {
 
-    public MainView() {
-        // Use TextField for standard text input
-        TextField textField = new TextField("Your name");
+  public MainView() {
+    addToNavbar(new DrawerToggle());
+    this.addToDrawer(new Button("Hello"));
 
-        // Button click listeners can be defined as lambda expressions
-        GreetService greetService = new GreetService();
-        Button button = new Button("Say hello",
-                e -> Notification.show(greetService.greet(textField.getValue())));
 
-        // Theme variants give you predefined extra styles for components.
-        // Example: Primary button is more prominent look.
-        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-
-        // You can specify keyboard shortcuts for buttons.
-        // Example: Pressing enter in this view clicks the Button.
-        button.addClickShortcut(Key.ENTER);
-
-        // Use custom CSS classes to apply styling. This is defined in shared-styles.css.
-        addClassName("centered-content");
-
-        add(textField, button);
-    }
+  }
 }
